1f1b960e8f24c70fc448c8a3908e2bb6fb5f4e0b
Radovan Paška <radovan.paska@avast.com>
2020-10-07 20:33:14 +0200

    fix database III

--- common/storage.py
+++ common/storage.py
@@ -7,7 +7,8 @@ import sqlite3
 def is_present():
     db = _open_database('data/presence_history.sqlite')
     cursor = db.cursor()
-    cursor.execute('select "present" from presence order by "timestamp" desc limit 0, 1')
+    cursor.row_factory = lambda cursor, row: row[0]
+    cursor.execute('select present from presence order by timestamp desc limit 0, 1')
 
     row = cursor.fetchone()[0]
     db.close()
@@ -18,14 +19,14 @@ def was_outside():
     db = _open_database('data/presence_history.sqlite')
     cursor = db.cursor()
     cursor.row_factory = lambda cursor, row: row[0]
-    cursor.execute('select present from presence order by "timestamp" desc limit 0, 5')
+    cursor.execute('select present from presence order by timestamp desc limit 0, 5')
 
     rows = cursor.fetchall()
     db.close()
 
     cnt = 0
     for row in rows:
-        if cnt == 0 and row == 0:
+        if cnt == 0 and row != 1:
             return False
         elif cnt >= 0 and row == 1:
             return False
@@ -41,7 +42,7 @@ def get_netatmo_data(column, count):
     db = _open_database('data/netatmo_history.sqlite')
     cursor = db.cursor()
     cursor.row_factory = lambda cursor, row: row[0]
-    cursor.execute('select {} from netatmo order by "timestamp" desc limit 0, {}'.format(column, count))
+    cursor.execute('select {} from netatmo order by timestamp desc limit 0, {}'.format(column, count))
 
     rows = cursor.fetchall()
     db.close()
