bd31f5f88eafbb5e8db369e9a6abdf47b9a4b8e4
Radovan Paška <radovan.paska@avast.com>
2020-10-24 14:43:45 +0200

    fix now

--- common/camera.py
+++ common/camera.py
@@ -6,7 +6,7 @@ import os
 import datetime
 
 def take_photo():
-    filedate = datetime.date.now().strftime("%Y_%m_%d_%H_%M")
+    filedate = datetime.datetime.now().strftime("%Y_%m_%d_%H_%M")
     capture = path.to('data/capture/capture_{}.jpeg'.format(filedate))
 
     if os.path.isfile(capture):
@@ -19,7 +19,7 @@ def take_photo():
         return
 
 def make_video():
-    filedate = datetime.date.today().strftime("%Y_%m_%d")
+    filedate = datetime.datetime.today().strftime("%Y_%m_%d")
     captures = path.to('data/capture/capture_{}*.jpeg'.format(filedate))
     video = path.to('data/video/video_{}.mp4'.format(filedate))
 
