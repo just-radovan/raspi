879a03c28236d325127b9b7aca7d4c053e1bed4c
Radovan Paška <radovan.paska@avast.com>
2020-12-13 22:14:49 +0000

    deduplicate twitter code

---
+++ common/twitter.py
@@ -0,0 +1,77 @@
+# -*- coding: UTF-8 -*-
+
+import path
+import common.log as log
+import auth.twitter as twitter
+
+import tweepy
+import json
+
+def id():
+    return None
+
+def tweet(access_data, message, media = None):
+    access_token = None
+    access_secret = None
+
+    with open(access_data) as infile:
+        data = json.load(infile)
+        for access in data['access']:
+            access_token = access['token']
+            access_secret = access['token_secret']
+
+    if not access_token or not access_secret:
+        log.error('tweet(): unable to load access tokens.')
+        return
+
+    auth = tweepy.OAuthHandler(twitter.get_consumer_key(), twitter.get_consumer_secret())
+    auth.set_access_token(access_token, access_secret)
+
+    api = tweepy.API(auth)
+
+    if media:
+        ids = []
+        if isinstance(media, list):
+            for media_item in media:
+                img = api.media_upload(media_item)
+                ids.append(img.media_id_string)
+        else:
+            img = api.media_upload(media)
+            ids.append(img.media_id_string)
+
+        api.update_status(status = message, media_ids = ids)
+        log.info('tweet(): "{}" w/ media'.format(message))
+    else:
+        api.update_status(status = message)
+        log.info('tweet(): "{}"'.format(message))
+
+def authorize(access_data):
+    auth = tweepy.OAuthHandler(twitter.get_consumer_key(), twitter.get_consumer_secret(), 'oob')
+    try:
+        redirect_url = auth.get_authorization_url()
+
+        print('👉 authorize(): to continue, please visit {}'.format(redirect_url))
+        verifier = input('verification code? ')
+    except tweepy.TweepError:
+        log.error('authorize(): failed to get authorization request token.')
+        return
+
+    try:
+        auth.get_access_token(verifier)
+    except tweepy.TweepError:
+        log.error('authorize(): failed to get access token.')
+        return
+
+    access_token = auth.access_token
+    access_secret = auth.access_token_secret
+
+    # store data
+    data = {}
+    data['access'] = []
+    data['access'].append({
+        'token': access_token,
+        'token_secret': access_secret
+    })
+
+    with open(access_data, 'w') as outfile:
+        json.dump(data, outfile)
--- common/twitter_prague.py
+++ common/twitter_prague.py
@@ -1,8 +1,7 @@
 # -*- coding: UTF-8 -*-
 
 import path
-import common.log as log
-import auth.twitter as twitter
+import common.twitter as twitter
 
 import tweepy
 import json
@@ -11,70 +10,9 @@ def id():
     return 'prague'
 
 def tweet(message, media = None):
-    accessToken = ''
-    accessTokenSecret = ''
-
-    with open(path.to('data/twitter_wp_access.data')) as infile:
-        data = json.load(infile)
-        for access in data['access']:
-            accessToken = access['token']
-            accessTokenSecret = access['token_secret']
-
-    if not accessToken or not accessTokenSecret:
-        log.error('tweet(): unable to load access tokens.')
-        quit()
-
-    auth = tweepy.OAuthHandler(twitter.get_consumer_key(), twitter.get_consumer_secret())
-    auth.set_access_token(accessToken, accessTokenSecret)
-
-    api = tweepy.API(auth)
-
-    if media:
-        ids = []
-        if isinstance(media, list):
-            for media_item in media:
-                img = api.media_upload(media_item)
-                ids.append(img.media_id_string)
-        else:
-            img = api.media_upload(media)
-            ids.append(img.media_id_string)
-
-        api.update_status(status = message, media_ids = ids)
-        log.info('tweet(): "{}" w/ media'.format(message))
-    else:
-        api.update_status(status = message)
-        log.info('tweet(): "{}"'.format(message))
+    access_data = path.to('data/twitter_access.data')
+    twitter.tweet(access_data, message, media)
 
 def authorize():
-    auth = tweepy.OAuthHandler(twitter.get_consumer_key(), twitter.get_consumer_secret(), 'oob')
-    try:
-        redirect_url = auth.get_authorization_url()
-
-        print('👉 authorize(): to continue, please visit {}'.format(redirect_url))
-        verifier = input('verification code? ')
-    except tweepy.TweepError:
-        log.error('authorize(): failed to get authorization request token.')
-        return
-
-    try:
-        auth.get_access_token(verifier)
-    except tweepy.TweepError:
-        log.error('authorize(): failed to get access token.')
-        return
-
-    accessToken = auth.access_token
-    accessSecret = auth.access_token_secret
-
-    log.info('authorize(): tokens: ' + accessToken + ', ' + accessSecret)
-
-    # store data
-
-    data = {}
-    data['access'] = []
-    data['access'].append({
-        'token': accessToken,
-        'token_secret': accessSecret
-        })
-
-    with open(path.to('data/twitter_wp_access.data'), 'w') as outfile:
-        json.dump(data, outfile)
+    access_data = path.to('data/twitter_access.data')
+    twitter.authorize(access_data)
