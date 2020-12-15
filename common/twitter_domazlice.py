7f30f59f1d70e358517876111e31591e62281b8f
Radovan Paška <radovan.paska@avast.com>
2020-12-15 18:22:24 +0000

    fix twitter scripts

--- common/twitter_domazlice.py
+++ common/twitter_domazlice.py
@@ -6,13 +6,15 @@ import common.twitter as twitter
 import tweepy
 import json
 
+auth_file = 'data/twitter_access_domazlice.data'
+
 def id():
     return 'domazlice'
 
 def tweet(message, media = None):
-    access_data = path.to('data/twitter_access.data')
+    access_data = path.to(auth_file)
     twitter.tweet(access_data, message, media)
 
 def authorize():
-    access_data = path.to('data/twitter_access.data')
+    access_data = path.to(auth_file)
     twitter.authorize(access_data)
